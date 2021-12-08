b51e6243ad59eca7da26b7111c6b2ee2f16f9391
Solarc117 <jonathan.carlos.bernal@gmail.com>
2021-12-07 17:28:03 -0700

    fetch data 📄

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ console.clear();
 function log() {
   console.log(...arguments);
 }
+
+d3.json(
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+)
+  .then(data => {
+    log(data);
+  })
+  .catch(err => console.error('❌', err));
