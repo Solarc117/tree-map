5bd6be6a496d55b09965d63d6855d77e1c85422d
Solarc117 <jonathan.carlos.bernal@gmail.com>
2021-12-21 17:13:12 -0700

    Final version 🎞😉

--- script.js
+++ script.js
@@ -5,81 +5,157 @@ function log() {
   console.log(...arguments);
 }
 
-const canvas = d3.select('.canvas'),
-  [canvasWidth, canvasHeight] = [
-    canvas.attr('width'),
-    canvas.attr('height'),
-  ].map(num => parseFloat(num));
+const paddingBottom = 38,
+  canvas = d3.select('.canvas'),
+  canvasWidth = parseFloat(canvas.attr('width')),
+  canvasHeight = parseFloat(canvas.attr('height')) - paddingBottom;
 
 d3.json(
   'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
 )
-  .then(data => {
-    /**
-    POTENTIALLY USEFUL METHODS:
-    (in this project, I'll try to only use methods that I find I really need, in order to avoid reading up on documentation that is hard to understand and therefore questionably useful, and also just to avoid unnecessary notes)
-    1. CSS 
-     - pointer-events
-    2. JS
-     - Array.filter((category, index,⚠ SELF ⚠) => {...})
-     - URLSearchParams objects
-       i) DEFAULT_DATASET
-       ii) instance.get()
-     - || (OR operator)
-     - D3
-       i) interpolateRgb()()
-       ii) d3.treemap()
-         - .size()
-         - .paddingInner()
-       iii) d3.hierarchy()
-         - .eachBefore()
-         - .sum() 📄 Potentially not D3
-         - .leaves()
-    @todo Define my treemap, and check that it is an array of objects. ✅
-    @todo Look up the syntax to define a treemap. ✅
-    @todo Define my treemap properly, and check that it is an array of objects. 
-    @todo Render a g element for each obj in the treemap. ✅
-    @todo Define the d3 color-scheme to be used. ⭐ 
-    @todo Color each rect according to its group.
-    📄 The value property represents the revenue in USD the movie raised.
-    */
-    // We tell D3 that the data is a tree structure, with the "Movies" node as the root and the individual movies as the 'leaf' nodes.
-    // 1st arg is the data, 2nd is a function that tells d3 where the children of each node are
+  .then(movies => {
     const hierarchy = d3
-      .hierarchy(data, node => node.children)
-      // Then we call the sum() method, which takes a function that specifies how to add a value to each node.
-      // Each leaf node has a value property. We want the larger values to correspond to larger tiles.
-      .sum(node => node.value)
-      // Then we want to use the sort() method, which accepts a function with two node parameters. For each node pair, we tell it how to sort them.
-      // This method is much like the native Array.sort() method; it returns a positive value to sort 2 before 1, a negative value to sort 1 before 2, and 0 to keep the current order.
-      // Here, if node2 has a greater value property, a positive value is returned and it is sorted BEFORE node1. We want the node with the highest value to come first.
-      .sort((node1, node2) => node2.value - node1.value);
-    // The hierarchy.leaves() method returns the leaf nodes of the tree obj.
+      .hierarchy(movies, ({ children }) => children)
+      .sum(movie => movie.value)
+      .sort((movie1, movie2) => movie2.value - movie1.value);
 
-    document.addEventListener('down', event => log(event.key));
-
-    // Now we can use the d3.treemap().size() methods to create a function that will render a treemap of the specified size with the passed hierarchical data.
-    const createTreemap = d3.treemap().size([canvasWidth, canvasHeight]);
-
-    const movieTree = createTreemap(hierarchy),
+    const createTreemap = d3.treemap().size([canvasWidth, canvasHeight]),
+      movieTree = createTreemap(hierarchy).leaves(), // Leaf nodes.
+      movieCategories = movieTree
+        .map(movieNode => movieNode.data.category)
+        .reduce(
+          (arr, curElem) => (arr.includes(curElem) ? arr : [...arr, curElem]),
+          []
+        ),
+      colorScheme = d3.scaleOrdinal(movieCategories, d3.schemeCategory10),
       cells = canvas
         .selectAll('.cell')
         .data(movieTree)
         .enter()
         .append('g')
-        .attr('class', 'cell')._groups[0];
+        .attr('id', ({ data: movie }) => movie.name)
+        .attr('data-category', ({ data: movie }) => movie.category)
+        .attr('data-revenue', ({ data: movie }) => movie.value)
+        .attr('class', 'cell')
+        ._groups[0].map(cell => d3.select(cell));
 
-    cells.forEach(cell =>
-      d3
-        .select(cell)
+    // Tooltip.
+    const tooltip = document.querySelector('.tooltip');
+    cells.forEach(cell => {
+      const movie = cell.data()[0].data;
+      cell
         .append('rect')
-        .attr('x', movieNode => movieNode.x0)
-        .attr('y', movieNode => movieNode.y0)
-        .attr('width', movieNode => movieNode.x1 - movieNode.x0)
-        .attr('height', movieNode => movieNode.y1 - movieNode.y0)
-        .style('stroke', 'black')
-        .style('stroke-width', '1px')
-        .style('fill', 'gold')
-    );
+        .attr('x', ({ x0 }) => x0)
+        .attr('y', ({ y0 }) => y0)
+        .attr('width', ({ x1, x0 }) => x1 - x0)
+        .attr('height', ({ y1, y0 }) => y1 - y0)
+        .style('fill', ({ data: movie }) => colorScheme(movie.category))
+        .on('mouseover', ({ target }) => showTooltip(target));
+      cell
+        .append('text')
+        .attr('x', ({ x0 }) => x0 + 3)
+        .attr('y', ({ y0 }) => y0 + 16) // Plus font-size.
+        .html(movieNode => {
+          const title = movieNode.data.name,
+            { x1, x0 } = movieNode,
+            rectWidth = x1 - x0,
+            g = document.getElementById(title),
+            textElem = g.children[1],
+            firstLineY = movieNode.y0 + 15;
+          function checkIfFits(str) {
+            textElem.innerHTML = str;
+            return textElem.getComputedTextLength() < rectWidth - 4;
+          }
+          function optimizeLines(title, lines = []) {
+            const words = Array.isArray(title)
+              ? title
+              : title
+                  .split(' ')
+                  .filter(s => s)
+                  .map(word =>
+                    checkIfFits(word) ? word : word.slice(0, 3) + '.'
+                  );
+            let currentLine = [];
+            while (
+              words.length > 0 &&
+              checkIfFits([currentLine.join(' '), words[0]].join(' '))
+            )
+              currentLine.push(words.shift());
+            lines.push(currentLine.join(' '));
+            return words.length > 0 ? optimizeLines(words, lines) : lines;
+          }
+          function renderText(linesArr) {
+            let str = ``;
+            linesArr.forEach(
+              (line, i) =>
+                (str += `<tspan x='${x0 + 2}' y='${
+                  firstLineY + i * 15
+                }'>${line}</tspan> `)
+            );
+            return str;
+          }
+          return renderText(optimizeLines(title));
+        })
+        .style('user-select', 'none')
+        .on('mouseover', ({ target }) => showTooltip(target));
+
+      function showTooltip(target) {
+        function addCommas(int) {
+          const arr = int.split(''),
+            subArrs = [];
+          for (let i = arr.length - 3; i > 0; i -= 3) {
+            subArrs.unshift(arr.splice(i, 3));
+          }
+          subArrs.unshift(arr);
+          return subArrs
+            .reduce((acc, curVal) => acc.concat(',', curVal))
+            .join('');
+        }
+        let { x, y } = target.getBoundingClientRect();
+        x += window.scrollX;
+        y += window.scrollY;
+
+        tooltip.innerHTML = `${movie.name} - $${addCommas(
+          movie.value
+        )} in revenue.`;
+
+        const { clientWidth: tooltipWidth, clientHeight: tooltipHeight } =
+          tooltip;
+
+        tooltip.style.left = `${x}px`;
+        tooltip.style.top = `${y}px`;
+      }
+    });
+
+    // Legend.
+    /*
+First, let me locate the y value I'll want for all the legend rects.
+legendx = canh + 4 ((38 - recth) / 2) (equal padding)
+Now, for the x, I'll just do 4 at first, then add 30 (rect), maybe 40 (cateogry title), and another 4 (for padding of next rect)
+*/
+    const legendCellWidth = canvasWidth / movieCategories.length,
+      legendRectSide = 20,
+      legendRectY = canvasHeight + (paddingBottom - legendRectSide) / 2;
+    const legendCells = canvas
+      .selectAll('.legend-cell')
+      .data(movieCategories)
+      .enter()
+      .append('g')
+      .attr('class', 'legend-cell')
+      ._groups[0].map(legendCell => d3.select(legendCell));
+    legendCells.forEach((cell, index) => {
+      cell
+        .append('rect')
+        .attr('x', () => index * legendCellWidth + 10)
+        .attr('y', legendRectY)
+        .attr('width', legendRectSide)
+        .attr('height', legendRectSide)
+        .style('fill', category => colorScheme(category));
+      cell
+        .append('text')
+        .attr('x', () => index * legendCellWidth + legendRectSide + 15)
+        .attr('y', legendRectY + 15)
+        .text(category => category);
+    });
   })
   .catch(err => console.error('❌', err));
